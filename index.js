9613c9782cc468c61000ae2669dfd89dddb5db3f
harison1224 <harisonochieng0@gmail.com>
2024-07-15 06:55:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-const totalBatteries = batteryBatches.reduce(function(total, batch) {
-    return total + batch;
-  }, 0);
-  
-  console.log(totalBatteries);
 
+const totalBatteries = batteryBatches.reduce(function(total, batches){
+  return batches + total
+  }
+)
